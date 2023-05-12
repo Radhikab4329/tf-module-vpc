4a3fb6217f98da0cd8827aa117a27e898eb6241f
Radhika <radhika.b4329@gmail.com>
2023-05-13 00:00:53 +0530

    update roboshop-infra

--- main.tf
+++ main.tf
@@ -20,7 +20,11 @@ resource "aws_subnet" "main" {
 
 resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
-  peer_vpc_id   = "vpc-0f2ecff9d1d4a7571"
+  peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
+  tags       = merge (
+    local.common_tags,
+    { Name = "${var.env}-peering" }
+  )
 }
