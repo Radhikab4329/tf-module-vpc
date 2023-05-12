9e3dd22c6613c19d8cad51f35828225e584f11f7
Radhika <radhika.b4329@gmail.com>
2023-05-12 23:47:08 +0530

    update roboshop-infra

--- main.tf
+++ main.tf
@@ -22,4 +22,5 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = "vpc-0f2ecff9d1d4a7571"
   vpc_id        = aws_vpc.main.id
+  auto_accept   = true
 }
