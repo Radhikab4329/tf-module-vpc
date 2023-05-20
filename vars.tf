c58c7517f46a23771157a5be0b99ce9d831bb28e
Radhika <radhika.b4329@gmail.com>
2023-05-20 17:52:55 +0530

    update vpc module

--- vars.tf
+++ vars.tf
@@ -1,9 +1,7 @@
 variable "cidr_block" {}
 variable "env" {}
-variable "public_subnets_cidr" {}
-variable "private_subnets_cidr" {}
 variable "default_vpc_id" {}
-variable "availability_zones" {}
+
 
 
 
