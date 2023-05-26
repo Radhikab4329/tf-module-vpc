cfae147e7cae56dafb33577e3534425c1ad173d5
Radhika <radhika.b4329@gmail.com>
2023-05-26 21:04:24 +0530

    update module subnet

--- main.tf
+++ main.tf
@@ -40,7 +40,8 @@ resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.ngw-eip.id
   subnet_id     = lookup(lookup(module.public_subnets, "public", null), "subnet_ids", null)[0]
 
-  tags       = merge (
+  tags = merge(
     local.common_tags,
     { Name = "${var.env}-ngw" }
   )
+}
