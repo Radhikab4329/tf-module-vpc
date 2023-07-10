c995648255cdee4006ab6e1fd53be3125e9b563c
Radhika <radhika.b4329@gmail.com>
2023-07-10 18:49:04 +0530

    docdb

--- subnets/main.tf
+++ subnets/main.tf
@@ -30,6 +30,7 @@ resource "aws_route_table" "route_table" {
 
 }
 
+
 resource "aws_route_table_association" "association" {
   count          = length(aws_subnet.main)
   subnet_id      = aws_subnet.main.*.id[count.index]
